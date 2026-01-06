c48aa32ce08116fd48ff95a0f32bdac89bab519c
Mnr04 <181124438+Mnr04@users.noreply.github.com>
2026-01-06 17:47:41 +0100

    jwt url init

--- softdesk/settings.py
+++ softdesk/settings.py
@@ -124,7 +124,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    # Gere la pagination afin d'optimiser le code
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import user_manager_view
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('users/', user_manager_view, name='user-create'),
     path('users/<int:pk>/', user_manager_view, name='user-detail'),
