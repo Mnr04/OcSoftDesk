c2f89098fe3c27e42ea5e4decd9d9d3d5e7af931
Mnr04 <181124438+Mnr04@users.noreply.github.com>
2026-01-08 19:35:35 +0100

    permissions

---
+++ projects/permissions.py
@@ -0,0 +1,37 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+class IsAuthorOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        if obj.author == request.user:
+            return True
+
+        return False
+
+
+class IsProjectContributor(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        from projects.models import Project, Issue, Comment
+
+        project_concerne = None
+
+        if isinstance(obj, Project):
+            project_concerne = obj
+
+        elif isinstance(obj, Issue):
+            project_concerne = obj.project
+
+        elif isinstance(obj, Comment):
+            project_concerne = obj.issue.project
+
+        if project_concerne is None:
+            return False
+
+        if project_concerne.author == request.user:
+            return True
+
+        if project_concerne.contributor_set.filter(user=request.user).exists():
+            return True
+        return False
