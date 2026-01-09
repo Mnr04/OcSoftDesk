66c25d83025d6c9227b6d933132b5f8f05de2329
Mnr04 <181124438+Mnr04@users.noreply.github.com>
2026-01-09 15:13:51 +0100

    User ModelViewSet

--- users/views.py
+++ users/views.py
@@ -1,41 +1,9 @@
-from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import User
 from .serializers import UserSerializer
 
-@api_view(['GET', 'POST', 'PUT', 'DELETE'])
-def user_manager_view(request, pk=None):
-
-    if pk is None:
-
-        if request.method == 'POST':
-            serializer = UserSerializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        elif request.method == 'GET':
-            users = User.objects.all()
-            serializer = UserSerializer(users, many=True)
-            return Response(serializer.data)
-
-    else:
-        user = get_object_or_404(User, pk=pk)
-
-        if request.method == 'GET':
-            serializer = UserSerializer(user)
-            return Response(serializer.data)
-
-        elif request.method == 'PUT':
-            serializer = UserSerializer(user, data=request.data, partial=True)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        elif request.method == 'DELETE':
-            user.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
