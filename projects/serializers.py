2878a4bb2af4675ff9a8e01088f606b025ede617
Mnr04 <181124438+Mnr04@users.noreply.github.com>
2026-01-09 15:27:45 +0100

    Project

--- projects/serializers.py
+++ projects/serializers.py
@@ -2,21 +2,8 @@ from rest_framework import serializers
 from .models import Project, Contributor, Issue, Comment
 
 class ProjectSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+
     class Meta:
         model = Project
         fields = ['id', 'title', 'description', 'type', 'author', 'created_time']
-
-class ContributorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Contributor
-        fields = ['id', 'user', 'project']
-
-class IssueSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Issue
-        fields = '__all__'
-
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = '__all__'
--- projects/urls.py
+++ projects/urls.py
@@ -1,17 +1,12 @@
-from django.urls import path
-from .views import project_view, contributor_view, issue_view, comment_view
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProjectViewSet
+
+router = DefaultRouter()
+
+router.register('projects', ProjectViewSet, basename='project')
+
 
 urlpatterns = [
-    path('projects/', project_view, name='project-list'),
-    path('projects/<int:pk>/', project_view, name='project-detail'),
-
-    path('contributors/', contributor_view, name='contributor-list'),
-    path('contributors/<int:pk>/', contributor_view, name='contributor-detail'),
-
-    path('issues/', issue_view, name='issue-list'),
-    path('issues/<int:pk>/', issue_view, name='issue-detail'),
-
-    path('comments/', comment_view, name='comment-list'),
-    path('comments/<int:pk>/', comment_view, name='comment-detail'),
-
+    path('', include(router.urls)),
 ]
